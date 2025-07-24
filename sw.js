df7054512953dd5f01d3a8024c5831fde0e3043d
collo670 <otienocollo95@gmail.com>
2025-07-24 12:46:51 +0000

    best pwa corrections

--- sw.js
+++ sw.js
@@ -1,7 +1,23 @@
-self.addEventListener('install', event => {
-  self.skipWaiting();
+const CACHE_NAME = 'ipray-v1';
+const ASSETS_TO_CACHE = [
+  '/i-prayer/',                      // Homepage
+  '/i-prayer/index.html',            // HTML file
+  '/i-prayer/dist/output.css',       // CSS
+  '/i-prayer/assets/images/bikira maria.jpg',
+  '/i-prayer/manifest.json',
+  // Add other critical assets
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => cache.addAll(ASSETS_TO_CACHE))
+  );
 });
 
-self.addEventListener('fetch', event => {
-  event.respondWith(fetch(event.request));
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => response || fetch(event.request))
+  );
 });
