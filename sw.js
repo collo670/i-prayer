cf893d58a1c56eab3292d9dda2e9bb74d67cdb14
collo670 <otienocollo95@gmail.com>
2025-07-25 10:41:04 +0000

    pwa suggestions

--- sw.js
+++ sw.js
@@ -4,8 +4,12 @@ const ASSETS_TO_CACHE = [
   '/i-prayer/index.html',            // HTML file
   '/i-prayer/dist/output.css',       // CSS
   '/i-prayer/assets/images/bikira maria.jpg',
+  '/i-prayer/assets/images/bikira-maria.png', // Apple touch icon from index.html
+  '/i-prayer/assets/images/icon-192x192.png', // Manifest icons
+  '/i-prayer/assets/images/icon-384x384.png',
+  '/i-prayer/assets/images/icon-512x512.png',
   '/i-prayer/manifest.json',
-  // Add other critical assets
+  // Add other critical assets as needed
 ];
 
 self.addEventListener('install', (event) => {
