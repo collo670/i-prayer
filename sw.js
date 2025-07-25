11de9a5cad3c2c552106152b4ce3e663b210cfe5
collo670 <otienocollo95@gmail.com>
2025-07-25 15:19:51 +0300

    pwa and icons

--- sw.js
+++ sw.js
@@ -1,21 +1,21 @@
 const CACHE_NAME = 'ipray-v1';
 const ASSETS_TO_CACHE = [
-  '/i-prayer/',                      // Homepage
-  '/i-prayer/index.html',            // HTML file
-  '/i-prayer/dist/output.css',       // CSS
-  '/i-prayer/assets/images/bikira maria.jpg',
-  '/i-prayer/assets/images/bikira-maria.png', // Apple touch icon from index.html
-  '/i-prayer/assets/images/icon-192x192.png', // Manifest icons
-  '/i-prayer/assets/images/icon-384x384.png',
-  '/i-prayer/assets/images/icon-512x512.png',
+  '/i-prayer/',
+  '/i-prayer/index.html',
+  '/i-prayer/dist/output.css',
   '/i-prayer/manifest.json',
-  // Add other critical assets as needed
+  '/i-prayer/assets/icons/icon-192x192.png',
+  '/i-prayer/assets/icons/icon-512x512.png',
+  '/i-prayer/assets/images/bikira-maria.jpg'
 ];
 
 self.addEventListener('install', (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME)
-      .then((cache) => cache.addAll(ASSETS_TO_CACHE))
+      .then((cache) => {
+        console.log('Caching assets');
+        return cache.addAll(ASSETS_TO_CACHE);
+      })
   );
 });
 
